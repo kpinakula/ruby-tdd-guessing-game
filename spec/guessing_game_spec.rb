191f22491d195aa726cc44b347ba8fb8035406d0
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-06-19 01:44:21 +0100

    simulate user input in first test to prevent test being interrupted by prompt

--- spec/guessing_game_spec.rb
+++ spec/guessing_game_spec.rb
@@ -5,8 +5,9 @@ describe 'guessing game' do
     secret_word = game.secret_word() + "\n"
 
     describe 'start guessing game' do
-        it 'outputs "guess the secret word" when run' do
-            expect { game.start() }.to output(/^guess the secret word/).to_stdout
+        it 'outputs "guess the secret word" and prompts user input when run' do
+            allow(game).to receive(:gets).and_return(secret_word)
+            expect { game.start }.to output(/^guess the secret word/).to_stdout
         end
         it 'prompts the user to enter a guess and returns true if it matches secret word' do
             allow(game).to receive(:gets).and_return(secret_word)
