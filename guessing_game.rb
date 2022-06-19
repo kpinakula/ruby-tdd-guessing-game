5bc981aa3ef82374d96c5fef0c337f1a1a154908
Korinna Pinakoulakis <kpinakoulaki@gmail.com>
2022-06-19 01:39:35 +0100

    prompt user input and check if it matches secret word

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,5 +1,19 @@
 class GuessingGame
+    def secret_word
+        "abracadabra"
+    end
+
+    def guess
+        gets.chomp
+    end
+
     def start()
         puts "guess the secret word"
+        guess = guess()
+        if guess == secret_word
+            true
+        else
+            false
+        end
     end
 end
--- spec/guessing_game_spec.rb
+++ spec/guessing_game_spec.rb
@@ -2,10 +2,15 @@ require_relative "../guessing_game.rb"
 
 describe 'guessing game' do
     game = GuessingGame.new
+    secret_word = game.secret_word() + "\n"
 
     describe 'start guessing game' do
         it 'outputs "guess the secret word" when run' do
             expect { game.start() }.to output(/^guess the secret word/).to_stdout
+        end
+        it 'prompts the user to enter a guess and returns true if it matches secret word' do
+            allow(game).to receive(:gets).and_return(secret_word)
+            expect(game.start).to eq true
         end
     end
 end
